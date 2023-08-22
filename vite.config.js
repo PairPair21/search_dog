bbbe55902bb96614028960cfeec4e6a787038df8
TU\5810612928 <pairchanunchida@gmail.com>
2023-08-23 03:56:21 +0700

    fixed error(2)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
   build: {
